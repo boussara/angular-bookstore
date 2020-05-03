36e195c1248a2eb6cb58fc33936ab0f38d920651
Mirage <you@example.cominventoory@gmail.com>
2020-05-03 15:52:42 +0000

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Book } from './common/book';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-bookstore';
+  books: Book[] = [
+    {
+      sku: 'text-100',
+      name: 'Python for SAS Users',
+      // tslint:disable-next-line: max-line-length
+      description: 'Business users familiar with Base SAS programming can now learn Python by example. You will learn via examples that map SAS programming constructs and coding patterns into their Python equivalents.',
+      unitPrice: 600,
+      imageUrl: 'assets/images/webdevelopment/text-100.jpg',
+      active: true,
+      unitsInStock: 100,
+      createdOn: new Date(),
+      updatedOn: null,
+    },
+    {
+      sku: 'text-101',
+      name: 'Deep Learning with JavaScript',
+      description: 'Deep learning has transformed the fields of computer vision, image processing, and natural language applications.',
+      unitPrice: 500,
+      imageUrl: 'assets/images/webdevelopment/text-101.jpg',
+      active: true,
+      unitsInStock: 100,
+      createdOn: new Date(),
+      updatedOn: null,
+    },
+    {
+      sku: 'text-102',
+      name: 'Clean Ruby',
+      // tslint:disable-next-line: max-line-length
+      description: 'Learn how to make better decisions and write cleaner Ruby code. This book shows you how to avoid messy code that is hard to test',
+      unitPrice: 700,
+      imageUrl: 'assets/images/webdevelopment/text-102.jpg',
+      active: true,
+      unitsInStock: 100,
+      createdOn: new Date(),
+      updatedOn: null,
+    }
+
+  ];
 }
